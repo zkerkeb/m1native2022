7f0e659cc0044139e1d50e3094fdd25484404ed7
Zakaria Kerkeb <hikkary@mac.local>
2023-03-10 14:59:33 +0100

    correction

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,14 +1,36 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useFocusEffect } from '@react-navigation/native';
-import React from 'react';
-
+import axios from 'axios';
+import React, { useEffect } from 'react';
+import { FlatList, Image } from 'react-native';
 import styled from 'styled-components';
 
-const Home = props => {
-  console.log('🚀 ~ file: home.js:6 ~ Home ~ props:', props);
+const BASE_URL = 'https://gateway.marvel.com/v1/public';
+const API_KEY = 'remplissez';
+const PRIVATE_KEY = 'remplissez';
+//  function generateHash(ts) {     const hash = md5(ts + PRIVATE_KEY + API_KEY);     return hash; }
 
+const CharacterRow = ({uriImage, name, description}) => {
+  return (
+    <>
+      <Image
+        style={{width: 100, height: 100}}
+        source={{
+          uri: uriImage,
+        }}
+      />
+      <StyledText>
+        {name} - {description}
+      </StyledText>
+    </>
+  );
+};
+
+const Home = props => {
+  const [page, setPage] = React.useState(0);
+  const [characters, setCharacters] = React.useState([]);
   useFocusEffect(() => {
-    AsyncStorage.removeItem('token');
+    // AsyncStorage.removeItem('token');
     AsyncStorage.getItem('token')
       .then(token => {
         if (!token) {
@@ -20,6 +42,35 @@ const Home = props => {
       });
   });
 
+  useEffect(() => {
+    getCharacters();
+  }, [page]);
+
+  const getCharacters = id => {
+    const ts = 1;
+    // const hash = generateHash(ts);
+    const hash = '3200d4b23a1b118f42bb12eeeaaa31e4';
+
+    axios
+      .get(`${BASE_URL}/characters`, {
+        params: {
+          apikey: API_KEY,
+          ts: ts,
+          hash: hash,
+          // populate: 'image',
+          limit: 20,
+          offset: page * 20,
+        },
+      })
+      .then(response => {
+        console.log('🚀 ~ file: home.js:6 ~ Home ~ response', response);
+        setCharacters([...characters, ...response.data.data.results]);
+      })
+      .catch(err => {
+        console.log('🚀 ~ file: home.js:6 ~ Home ~ err', err);
+      });
+  };
+
   const handleNavigation = page => {
     props.navigation.navigate(page);
   };
@@ -29,6 +80,23 @@ const Home = props => {
       <StyledButton onPress={() => handleNavigation('Login')}>
         <StyledText>To Login</StyledText>
       </StyledButton>
+      <FlatList
+        data={characters}
+        onEndReached={() => {
+          console.log('end reached');
+          setPage(page + 1);
+        }}
+        renderItem={({item}) => (
+          <CharacterRow
+            name={item.name}
+            description={item.description}
+            uriImage={`${item.thumbnail?.path.replace('http', 'https')}.${
+              item.thumbnail?.extension
+            }`}
+          />
+        )}
+        keyExtractor={item => item.id}
+      />
     </Container>
   );
 };
